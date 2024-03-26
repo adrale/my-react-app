9a6d3479036ae35adf52f99b4ff5e8346a3c9bef
ALEXANDRE MORO <alexandre.moro@live.fr>
2024-03-26 16:55:58 +0100

    finish react basics 4

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,23 +1,11 @@
-function PokemonCard() {
-    const pokemonList = [
-      {
-        name: "bulbasaur",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      },
-      {
-        name: "mew",
-      },
-    ];
-
-    const pokemon = pokemonList[0]
+function PokemonCard({pokemon}) {
     return (
       <figure>
         {pokemon.imgSrc ? (
           <img src={pokemon.imgSrc} alt={pokemon.name} />
-        ) : (
-          <p>???</p>
-        )
+          ) : (
+            <p>???</p>
+          )
         }
         <figcaption>{pokemon.name}</figcaption>
       </figure>
