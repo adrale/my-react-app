7ac1feda9e168ba861e505a749fee1b80e2cecc3
ALEXANDRE MORO <alexandre.moro@live.fr>
2024-03-26 14:50:21 +0100

    finish react bascis 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,28 @@
 function PokemonCard() {
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-            <figcaption>Balbusaur</figcaption>
-        </figure>
-    );
+    const pokemonList = [
+      {
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
+
+    const pokemon = pokemonList.map((pokemon, index) => (
+      pokemon.imgSrc ? 
+      <figure key={index}> 
+        <img src={pokemon.imgSrc} alt={pokemon.name}/>
+        <figcaption>{pokemon.name}</figcaption>
+      </figure> 
+      : 
+      <figure key={index}>
+        <p>{pokemon.imgSrc = '???'}</p>
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    ));
+
+    return pokemon;
 }
 export default PokemonCard;
